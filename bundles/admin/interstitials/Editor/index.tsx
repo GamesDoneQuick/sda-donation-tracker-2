RTAinJapan/donation-tracker

wip - correct paths for interstitial editor to avoid redirects

bundles/admin/interstitials/Editor/index.tsx
@@ -25,8 +25,8 @@ export default function InterstitialEditor() {
   const fetchAll = useCallback(() => {
     setPromise(
       Promise.all([
-        fetch(`${API_ROOT}interviews/${event}`).then(JSONResponseWithForbidden),
-        fetch(`${API_ROOT}ads/${event}`).then(JSONResponseWithForbidden),
+        fetch(`${API_ROOT}interviews/${event}/`).then(JSONResponseWithForbidden),
+        fetch(`${API_ROOT}ads/${event}/`).then(JSONResponseWithForbidden),
         fetch(`${API_ROOT}search?type=run&event=${event}`).then(JSONResponse),
         fetch(`${API_ROOT}me`).then(JSONResponseWithForbidden),
       ]),

GamesDoneQuick/sda-donation-tracker-2

bundles/admin/interstitials/Editor/index.tsx
@@ -26,8 +26,8 @@ export default function InterstitialEditor() {
   const fetchAll = useCallback(() => {
     setPromise(
       Promise.all([
-        fetch(`${API_ROOT}interviews/${eventId}`).then(JSONResponseWithForbidden),
-        fetch(`${API_ROOT}ads/${eventId}`).then(JSONResponseWithForbidden),
+        fetch(`${API_ROOT}interviews/${eventId}/`).then(JSONResponseWithForbidden),
+        fetch(`${API_ROOT}ads/${eventId}/`).then(JSONResponseWithForbidden),
         fetch(`${API_ROOT}search?type=run&event=${eventId}`).then(JSONResponse),
         fetch(`${API_ROOT}me`).then(JSONResponseWithForbidden),
       ]),